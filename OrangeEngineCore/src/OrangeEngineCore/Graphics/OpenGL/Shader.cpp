759eabfb13393474d9976bccc1b2ab209f37d01e
Alexander <sqcode06@gmail.com>
2021-09-28 11:40:05 +0300

    Added shader class

---
+++ OrangeEngineCore/src/OrangeEngineCore/Graphics/OpenGL/Shader.cpp
@@ -0,0 +1,110 @@
+#include "OrangeEngineCore/Graphics/OpenGL/Shader.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <spdlog/spdlog.h>
+
+namespace OrangeEngine
+{
+	bool create_shader(const char* source, const GLenum shader_type, GLuint& shader_id)
+	{
+		shader_id = glCreateShader(shader_type);
+		glShaderSource(shader_id, 1, &source, nullptr);
+		glCompileShader(shader_id);
+
+		GLint success;
+		glGetShaderiv(shader_id, GL_COMPILE_STATUS, &success);
+		if (success == GL_FALSE)
+		{
+			char info_log[1024];
+			glGetShaderInfoLog(shader_id, 1024, nullptr, info_log);
+
+			spdlog::critical("Shader compilation error:\n{}", info_log);
+			return false;
+		}
+		return true;
+	}
+
+	Shader::Shader(const char* vertex_shader_src, const char* fragment_shader_src)
+	{
+		GLuint vertex_shader_id = 0;
+		if (!create_shader(vertex_shader_src, GL_VERTEX_SHADER, vertex_shader_id))
+		{
+			spdlog::critical("Vertex shader: compile error!");
+			glDeleteShader(vertex_shader_id);
+			return;
+		}
+
+		GLuint fragment_shader_id = 0;
+		if (!create_shader(fragment_shader_src, GL_FRAGMENT_SHADER, fragment_shader_id))
+		{
+			spdlog::critical("Fragment shader: compile error!");
+			glDeleteShader(vertex_shader_id);
+			glDeleteShader(fragment_shader_id);
+			return;
+		}
+
+		m_id = glCreateProgram();
+		glAttachShader(m_id, vertex_shader_id);
+		glAttachShader(m_id, fragment_shader_id);
+		glLinkProgram(m_id);
+
+		GLint success;
+		glGetProgramiv(m_id, GL_LINK_STATUS, &success);
+		if (success == GL_FALSE)
+		{
+			GLchar info_log[1024];
+			glGetProgramInfoLog(m_id, 1024, nullptr, info_log);
+			spdlog::critical("SHADER PROGRAM: Link-time error:\n{0}", info_log);
+			glDeleteProgram(m_id);
+			m_id = 0;
+			glDeleteShader(vertex_shader_id);
+			glDeleteShader(fragment_shader_id);
+			return;
+		}
+		else
+		{
+			m_isCompiled = true;
+		}
+
+		glDetachShader(m_id, vertex_shader_id);
+		glDetachShader(m_id, fragment_shader_id);
+		glDeleteShader(vertex_shader_id);
+		glDeleteShader(fragment_shader_id);
+	}
+
+	Shader::~Shader()
+	{
+		glDeleteProgram(m_id);
+	}
+
+	void Shader::bind() const
+	{
+		glUseProgram(m_id);
+	}
+
+	void Shader::unbind()
+	{
+		glUseProgram(0);
+	}
+
+	Shader& Shader::operator=(Shader&& shader)
+	{
+		glDeleteProgram(m_id);
+		m_id = shader.m_id;
+		m_isCompiled = shader.m_isCompiled;
+
+		shader.m_id = 0;
+		shader.m_isCompiled = false;
+		return *this;
+	}
+
+	Shader::Shader(Shader&& shader)
+	{
+		m_id = shader.m_id;
+		m_isCompiled = shader.m_isCompiled;
+
+		shader.m_id = 0;
+		shader.m_isCompiled = false;
+	}
+}
---
+++ OrangeEngineCore/src/OrangeEngineCore/Graphics/OpenGL/Shader.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace OrangeEngine
+{
+	class Shader
+	{
+	public:
+		Shader(const char* vertex_shader_src, const char* fragment_shader_src);
+		Shader(Shader&&);
+		Shader& operator=(Shader&&);
+		~Shader();
+
+		Shader() = delete;
+		Shader(const Shader&) = delete;
+		Shader& operator=(const Shader&) = delete;
+
+		void bind() const;
+		static void unbind();
+		bool isCompiled() const { return m_isCompiled; }
+
+	private:
+		bool m_isCompiled = false;
+		unsigned int m_id = 0;
+	};
+}
